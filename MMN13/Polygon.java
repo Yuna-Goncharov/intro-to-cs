4d9eb8aa49ed2e6538eb4d31dcb5e250279b3477
Eyal Cohen <eyal@devsense.co.il>
2017-05-12 15:18:20 +0300

    Calc area and peri

--- MMN13/Polygon.java
+++ MMN13/Polygon.java
@@ -17,7 +17,7 @@ public class Polygon {
      * @return vertex - the highest point
      */
     public Point highestVertex() {
-        Point highestPoint = _noOfVertices > 0 ? _vertices[0] : null;
+        Point highestPoint = _vertices[0];
         
         for (int index = 0; index < _noOfVertices; index++) {
             if (_vertices[index].isAbove(highestPoint)) {
@@ -37,32 +37,31 @@ public class Polygon {
      * @return true if the adding was successfull, false if the Points array is full.
      */
     public boolean addVertex(double x, double y) {
-        for (int index = 0; index <= _noOfVertices; index++) {
-            if (_vertices[index] == null) {
-                _vertices[index] = new Point(x, y);
-                _noOfVertices += 1;
-                
-                return true;
-            } 
-        }
+        if (_noOfVertices == VERTICES_MAX) {
+            return false;
+        } 
         
-        return false;
+        _vertices[_noOfVertices++] = new Point(x, y);
+        
+        return true;
     }
     
     @Override
     public String toString() {
-        String str = "";
+        String string = "";
         if ( _noOfVertices > 0 ) {
-            str = "The polygon has " + _noOfVertices + " vertices:\n(" + _vertices[0];
+            string = "The polygon has " + _noOfVertices + " vertices:\n(" + _vertices[0];
             
-            for ( int i = 1; i < _noOfVertices; i++ ) { 
-                str += "," + _vertices[i];  
-                str += ")";
+            for ( int i = 0; i < _noOfVertices; i++ ) { 
+                string += "," + _vertices[i];  
+                if (i == (_noOfVertices - 1)) {
+                    string += ")";
+                }
             }
         } else {
-            str = "The polygon has 0 vertices.";
+            string = "The polygon has 0 vertices.";
         }
-        return str;		
+        return string;     
     }
     
     /**
@@ -71,7 +70,22 @@ public class Polygon {
      * @return perimeter
      */
     public double calcPerimeter() {
-        return 0;
+        double peri = 0;
+        Point[] vertices = getVertices();
+        
+        if (_noOfVertices <= 1) {
+            return peri;
+        }
+        
+        if (_noOfVertices == 2) {
+            return vertices[0].distance(vertices[1]);
+        }
+        
+        for (int i = 0; i < vertices.length - 1; i++) {
+            peri += vertices[i].distance(vertices[i+1]);
+        }
+        
+        return peri;
     }
     
     /**
@@ -80,7 +94,18 @@ public class Polygon {
      * @return area
      */
     public double calcArea() {
-        return 0;
+        double area = 0;
+        Point[] vertices = getVertices();
+            
+        if (_noOfVertices > 3) {
+            for (int i = 0; i < _noOfVertices; i++) {
+                if (vertices[i] != null) { 
+                    area += heron(vertices[0], vertices[i], vertices[i + 1]);
+                }
+            }
+        }
+        
+        return area;
     }
     
     /**
@@ -90,7 +115,7 @@ public class Polygon {
      * @retrun true if this is bigger, false if other is bigger.
      */
     public boolean isBigger(Polygon other) {
-        return false;
+        return calcArea() > other.calcArea();
     }
     
     /**
@@ -140,6 +165,58 @@ public class Polygon {
      * @return retangle
      */
     public Polygon getBoundingBox() {
-        return new Polygon();
+        if (_noOfVertices < 3) {
+            return null;
+        }
+        
+        Polygon box;
+        Point maxY =  highestVertex();
+        Point minY = _vertices[0];
+        Point maxX = _vertices[0];
+        Point minX = _vertices[0];
+        
+        for (int i = 0; i < _noOfVertices; i++) {
+            Point point = _vertices[i];
+           if (point.isUnder(minY)) {
+                minY = point;
+            } else if (point.isRight(maxX)) {
+                maxX = point;
+            } else if (point.isLeft(minX)) {
+                minX = point;
+            }
+        }
+        
+        box = new Polygon();
+        box.addVertex( minX.getX(), minY.getY() );
+        box.addVertex( minX.getX(), maxY.getY() );
+        box.addVertex( maxX.getX(), maxY.getY() );
+        box.addVertex( maxX.getX(), minY.getY() );
+        
+        return box;
+    }
+    
+    private Point[] getVertices() {
+        if (_noOfVertices <= 1) {
+            return new Point[0];
+        } 
+        
+        Point[] vertices = new Point[_noOfVertices + 1];
+        
+        for (int i = 0; i < _noOfVertices; i++) {
+            vertices[i] = _vertices[i];
+        }
+        vertices[_noOfVertices] = _vertices[0]; // Connect the last one with the first one
+        
+        return vertices;
+    }
+    
+    private double heron(Point a, Point b, Point c) {
+        double AB = a.distance(b);
+        double BC = b.distance(c);
+        double CA = c.distance(a);
+        
+        double s = (AB + BC + CA) / 2;
+        
+        return Math.sqrt(s * (s - AB) * (s - BC) * (s - CA));
     }
 }
