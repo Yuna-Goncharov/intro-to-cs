d15fd02cf272e5a028ce661a374748cde14bda5d
Eyal Cohen <eyal@devsense.co.il>
2017-06-07 22:12:49 +0300

    Start implement isSink

--- MMN14/Ex14.java
+++ MMN14/Ex14.java
@@ -3,6 +3,37 @@ import java.util.Arrays;
 public class Ex14 {
     
     public static int isSink(int [][] mat) {
+        int sink = -1;
+        int mightBe;
+        int row = 0;
+        int col = 0;
+
+        while (row <= mat.length - 1 && col <= mat.length - 1) {
+            if (mat[row][col] == 0) {
+                col++;
+
+                if (col == mat.length) {
+                    sink = col;
+                }
+            } else if (mat[row][col] == 1) {
+                row++;
+            }
+        }
+
+        if (sink >= 0) { 
+            for (int i = 0; i < mat.length; i++) {
+                System.out.println(sink);
+                // System.out.println(mat[sink]);
+                // if (mat[sink][i] == 1) {
+                    // sink = row;
+                // } 
+            }
+        }
+
+        if (sink >= 0) {
+            return sink;
+        }
+
         return -1;
     }
     
@@ -12,64 +43,69 @@ public class Ex14 {
         int rowHigh = mat.length;
         int colHigh = mat.length;
 
+        boolean isFound = false;
         if (mat[rowLow][colLow] <= x && mat[rowHigh - 1][colHigh - 1] >= x) {
 
-            while (rowLow < rowHigh && colLow < colHigh ) {
+            while (!isFound && rowLow < rowHigh && colLow < colHigh ) {
                 int rowMid = (rowLow + rowHigh) / 2;
                 int colMid = (colLow + colHigh) / 2;
-                // printMat(rowHigh, rowLow, rowMid, colHigh, colLow, colMid);
+
                 // First cell forth quarter
-                if (mat[rowMid][colMid] == x) {
-                    return true; 
-                } 
-                
-                if (mat[rowMid][colMid] > x) {
-                    // First cell third quarter
-                    if (mat[rowMid][colLow] == x) {
-                        return true;
+                if (!(rowHigh - 1 == rowLow && colHigh - 1 == colLow)) { 
+                    if (mat[rowMid][colMid] == x) {
+                        return true; 
                     } 
                     
-                    if (mat[rowMid][colLow] > x) {
-                        // First cell second quarter
-                        if (mat[rowLow][colMid] == x) {
+                    if (mat[rowMid][colMid] > x) {
+                        // First cell third quarter
+                        if (mat[rowMid][colLow] == x) {
                             return true;
-                        }
+                        } 
                         
-                        if (mat[rowLow][colMid] > x) {
-
-                            // First cell first quarter
-                            if (mat[rowLow][colLow] == x) {
+                        if (mat[rowMid][colLow] > x) {
+                            // First cell second quarter
+                            if (mat[rowLow][colMid] == x) {
                                 return true;
                             }
                             
-                            if (mat[rowLow][colLow] < x) {
-                                // System.out.println("After");
-                                printMat(rowHigh, rowLow, rowMid, colHigh, colLow, colMid);
-                                // Here is the problem
-                                rowHigh = rowMid + 1;
-                                colHigh = colMid;
+                            if (mat[rowLow][colMid] > x) {
+
+                                // First cell first quarter
+                                if (mat[rowLow][colLow] == x) {
+                                    return true;
+                                }
+                                
+                                if (mat[rowLow][colLow] < x) {
+                                    // Here is the problem
+                                    rowHigh = rowMid + 1;
+                                    colHigh = colMid;
+                                }
+                            } 
+                            
+                            if (mat[rowLow][colMid] < x) {
+                                colLow  = colMid;
+                                rowHigh = rowMid;
                             }
                         } 
                         
-                        if (mat[rowLow][colMid] < x) {
-                            colLow  = colMid;
-                            rowHigh = rowMid;
-                        }
+                        if (mat[rowMid][colLow] < x) {
+                            colHigh = colMid;
+                            rowLow  = rowMid;
+                        } 
                     } 
                     
-                    if (mat[rowMid][colLow] < x) {
-                        colHigh = colMid;
-                        rowLow  = rowMid;
-                    } 
-                } 
-                
-                if (mat[rowMid][colMid] < x) {
-                    colLow = colMid;
-                    rowLow = rowMid;
+                    if (mat[rowMid][colMid] < x) {
+                        colLow = colMid;
+                        rowLow = rowMid;
+                    }
+                } else {
+                    if (mat [rowHigh - 1] [colHigh - 1] == x) 
+                        return true;
+                    else
+                        return false;
                 }
-            }
+            } 
         }
-        
         return false; 
     }
     public static boolean isSumOf(int [] s, int n) {
