883e95aa48c20d033ae694933e7fa65c8a0fa15e
Eyal Cohen <eyalcohen4.ec@gmail.com>
2017-06-24 18:52:33 +0300

    Add comments to PointNode

--- MMN15/PointNode.java
+++ MMN15/PointNode.java
@@ -1,32 +1,72 @@
 public class PointNode {
     Point _point;
     PointNode _next;
-    
+	
+    /** 
+     * Constructor For PointNode class, instanciate both point and next
+     *
+     * @param p - the point to store at PointNode	
+     * @param n - the next PointNode	
+     */    
     public PointNode(Point p, PointNode n) {
         _point = new Point(p);
         _next = n;
     }
 
+
+    /** 
+     * Constructor For PointNode class, instanciate point only
+     * @param p - the point to store at PointNode	
+     *
+     */
     public PointNode(Point p) {
         _point = new Point(p);
     }
 
+	
+    /** 
+     * Constructor For PointNode class, instanciate next only
+     *
+     * @param n - the next PointNode
+     *
+     */
     public PointNode(PointNode n) {
         _next = n;
     }
 
+
+    /** 
+     * Retrive the point
+     *
+     * @return Point 
+     */
     public Point getPoint() {
         return new Point(_point);
     }
 
+
+    /** 
+     * Retrive the next PointNode
+     *
+     * @return PointNode 
+     */
     public PointNode getNext() {
         return _next;
     }
 
+	
+    /** 
+     * Set the point
+     *
+     */
     public void setPoint(Point p) {
         _point = new Point(p);
     }
-
+    
+    /** 
+     * Set the next
+     *
+     */
     public void setNext(PointNode next) {
         _next = next;
     }
