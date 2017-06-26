fdc7e07559ca1508ab95ddf6be8d1276dc960ce5
Eyal Cohen <eyal@devsense.co.il>
2017-06-26 15:07:15 +0300

    Add comment

--- exams/CharListTesting.java
+++ exams/CharListTesting.java
@@ -3,13 +3,14 @@ public class CharListTesting {
         CharList list = new CharList();
         CharList sublist = new CharList();
 
+	// We add to the add, so it may be the problem - the sublist should be reverse
         list.add(new CharNode('a'));
         list.add(new CharNode('b'));
         list.add(new CharNode('c'));
         list.add(new CharNode('d'));
 
-        sublist.add(new CharNode('a'));
         sublist.add(new CharNode('b'));
+        sublist.add(new CharNode('a'));
 
         int counter = list.subList(sublist);
 	System.out.println(counter);
